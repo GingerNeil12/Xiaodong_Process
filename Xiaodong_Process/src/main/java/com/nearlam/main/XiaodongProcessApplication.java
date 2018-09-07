4b6dd760043c960508745c53836049d85b004736
40281750 <40281750@ME0D002-0180E3.c27-merch.napier.ac.uk>
2018-09-07 11:55:39 +0100

    Initial Commit

---
+++ Xiaodong_Process/src/main/java/com/nearlam/main/XiaodongProcessApplication.java
@@ -0,0 +1,12 @@
+package com.nearlam.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaodongProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaodongProcessApplication.class, args);
+	}
+}
